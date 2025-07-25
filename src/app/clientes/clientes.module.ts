bd953ab70f4c28f3ca400ad619ee62f11886aca9
enzogl7 <enzolima527@gmail.com>
2025-07-25 14:43:42 -0300

    finalização crud de clientes

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'
 
 import { ClientesRoutingModule } from './clientes-routing.module';
+import { ClientesFormComponent } from './clientes-form/clientes-form.component';
+import { ClientesListaComponent } from './clientes-lista/clientes-lista.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ClientesFormComponent, ClientesListaComponent],
   imports: [
     CommonModule,
-    ClientesRoutingModule
-  ]
+    ClientesRoutingModule,
+    FormsModule
+  ],
+  exports: [ClientesFormComponent, ClientesListaComponent]
 })
 export class ClientesModule { }
